511bf0b06d20eeb6da98d6336053c41d54113d4a
Karolis Baukas <KarolisBaukas5@gmail.com>
2020-03-28 20:01:05 +0200

    Updates

---
+++ precache-manifest.1ef545ca555acc9e24b5805e8657dac3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ca77d964187874750b13c9b3398dde",
+    "url": "/React-pokemon/index.html"
+  },
+  {
+    "revision": "482eb2ef95a3585399a1",
+    "url": "/React-pokemon/static/css/main.2bb2371d.chunk.css"
+  },
+  {
+    "revision": "c236baca2c8d657b0eca",
+    "url": "/React-pokemon/static/js/2.8fb1988c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-pokemon/static/js/2.8fb1988c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482eb2ef95a3585399a1",
+    "url": "/React-pokemon/static/js/main.60f65b9c.chunk.js"
+  },
+  {
+    "revision": "ff8bc98956e0eb4231af",
+    "url": "/React-pokemon/static/js/runtime-main.325beb0b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-pokemon/static/media/logo.5d5d9eef.svg"
+  }
+]);
